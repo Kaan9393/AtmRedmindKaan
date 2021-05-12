e959d0928d4851ff33c538884f1a65101c37b73a
Kaan <snobby_snobby@hotmail.com>
2021-05-12 20:12:51 +0200

    Initial commit

---
+++ ATM.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtmRedmindKaan
+{
+    public class ATM
+    {
+        public int ThousandBill { get; set; }
+        public int FivehundredBill { get; set; }
+        public int OnehundredBill { get; set; }
+        public int RemainingTotalAmount => (ThousandBill * 1000) + (FivehundredBill * 500) + (OnehundredBill * 100);
+
+
+
+        public bool AtmCalculate(int amount)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"Amount to Withdraw {amount}");
+            Console.WriteLine($"Remaining blance in account {RemainingTotalAmount}");
+            Console.WriteLine($"Remaining Thousandbills [{ThousandBill}x]");
+            Console.WriteLine($"Remaining Fivehundredbills [{FivehundredBill}x]");
+            Console.WriteLine($"Remaining Onehundredbills [{OnehundredBill}x]\n\n");
+
+            int UsedThousandbill = 0;
+            int UsedFiveHundredBill = 0;
+            int UsedOneHundredBill = 0;
+
+
+            if (RemainingTotalAmount == 0 || RemainingTotalAmount < amount)
+            {
+                Console.WriteLine("Not enough balance in account");
+                return false;
+            }
+
+            //Lägg while i en metod, som tex returnerar console writeline
+            while (amount != 0 && RemainingTotalAmount != 0)
+            {
+                if (amount >= 1000 && ThousandBill != 0)
+                {
+                    amount -= 1000;
+                    UsedThousandbill++;
+                    ThousandBill -= 1;
+                    Console.WriteLine($"Tried to withdrawal: 1x - 1000\nWithdrawal left: {amount}");
+
+                }
+
+                else if (amount >= 500 && FivehundredBill != 0)
+                {
+                    amount -= 500;
+                    UsedFiveHundredBill++;
+                    FivehundredBill -= 1;
+                    Console.WriteLine($"Tried to withdrawal: 1x - 500\nWithdrawal left: {amount}");
+                }
+
+
+                else if (amount >= 100 && OnehundredBill != 0)
+                {
+                    amount -= 100;
+                    UsedOneHundredBill++;
+                    OnehundredBill--;
+                    Console.WriteLine($"Tried to withdrawal: 1x - 100\nWithdrawal left: {amount}");
+
+                }
+                else if (amount == 0)
+                {
+                    Console.WriteLine("Withdrawal successfully done");
+                    return true;
+                }
+
+                else
+                {
+                    ThousandBill += UsedThousandbill;
+                    FivehundredBill += UsedFiveHundredBill;
+                    OnehundredBill += UsedOneHundredBill;
+                    Console.WriteLine("\nWithdraw failed\n*Reset of amounts 'tried withdrawals' is completed!");
+                    return false;
+                }
+
+            }
+            Console.WriteLine("-----------------------------------------");
+
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtmRedmindKaan
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ATM atm = new ATM()
+            {
+                ThousandBill = 2,
+                FivehundredBill = 3,
+                OnehundredBill = 5
+            };
+
+            int[] WithdrawArray = new int[] { 1500, 700, 400, 1100, 1000, 700, 300 };
+
+            for (int i = 0; i < WithdrawArray.Length; i++)
+            {
+                atm.AtmCalculate(WithdrawArray[i]);
+            }
+        }
+    }
+}
+
+
+
+
